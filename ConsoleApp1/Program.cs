b286b046602b3f3837f4758b331f1c9ff945f8b6
Guilherme Henrique <108905023+oisouguilherme@users.noreply.github.com>
2023-03-02 10:07:32 -0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite sua idade");
+        int idade = int.Parse(Console.ReadLine());
+
+        if (idade>=0 && idade <=11)
+        {
+            Console.WriteLine("É criança");
+        } else if (idade >=12 && idade <=18)
+        {
+            Console.WriteLine("É adolescente");
+        } else if (idade >= 19 && idade <= 60)
+        {
+            Console.WriteLine("É adulto");
+        } else
+        {
+            Console.WriteLine("É idoso");
+        }
+        Console.ReadLine();
+        
+    }
+}
